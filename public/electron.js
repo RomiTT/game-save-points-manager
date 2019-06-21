d2acbe081246f672a4968bf9d35218c91cb15153
RomiTT <chikyom.kim@gmail.com>
2019-06-21 23:23:44 +0900

    Wrote supporting dev-tool extension for electron.

--- public/electron.js
+++ public/electron.js
@@ -25,6 +25,45 @@ function createWindow() {
 
   if (isDev) {
     mainWindow.webContents.openDevTools();
+
+    const os = require('os');
+    const { readdirSync } = require('fs');
+
+    const REACT_DEV_TOOL_ID = 'fmkadmapgofadopljbjfkapdkoienihi';
+    const REDUX_DEV_TOOL_ID = 'lmhkpmbekcpmknklioeibfkpmmfibljd';
+    const MOBX_DEV_TOOL_ID = 'pfgnfdagidkfgccljigdamigbcnndkod';
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+
+    const extensionPath = path.join(
+      os.homedir(),
+      '/AppData/Local/Google/Chrome/User Data/Default/Extensions'
+    );
+
+    // React Dev Tool
+    var version = readdirSync(`${extensionPath}/${REACT_DEV_TOOL_ID}`)[0];
+    if (version) {
+      BrowserWindow.addDevToolsExtension(
+        `${extensionPath}/${REACT_DEV_TOOL_ID}/${version}`
+      );
+    }
+
+    // Redux Dev Tool
+    version = readdirSync(`${extensionPath}/${REDUX_DEV_TOOL_ID}`)[0];
+    if (version) {
+      BrowserWindow.addDevToolsExtension(
+        `${extensionPath}/${REDUX_DEV_TOOL_ID}/${version}`
+      );
+    }
+
+    // Mobx Dev Tool
+    version = readdirSync(`${extensionPath}/${MOBX_DEV_TOOL_ID}`)[0];
+    if (version) {
+      BrowserWindow.addDevToolsExtension(
+        `${extensionPath}/${MOBX_DEV_TOOL_ID}/${version}`
+      );
+    }
   }
 
   mainWindow.on('closed', () => {
