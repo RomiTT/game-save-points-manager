9f3c8dd8176b29e96153633c57b815894c652b38
RomiTT <chikyom.kim@gmail.com>
2019-06-24 03:31:27 +0900

    Moved the location of FramelessTtlebar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TitleBar, TitleBarTheme } from './FramelessTitlebar';
+import { TitleBar, TitleBarTheme } from './components/FramelessTitlebar';
 
 import {
   Navbar,
