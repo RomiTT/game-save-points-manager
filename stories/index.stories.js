72ad68129f561b230bcd7a05dbf99db4ffbf850c
RomiTT <chikyom.kim@gmail.com>
2019-06-25 02:28:45 +0900

    Wrote code for testing the custom NavBar.

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -5,15 +5,53 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
+import {
+  NavBar,
+  NavBarLeftItem,
+  NavBarCenterItem,
+  NavBarRightItem
+} from './css_tutorials/NavBar';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('CSS tutorials', module).add('NavBar using flexbox', () => (
+  <div>
+    <NavBar>
+      <NavBarLeftItem>
+        <div>Left Contents =============================</div>
+      </NavBarLeftItem>
+      <NavBarCenterItem>
+        <div>Center Item</div>
+      </NavBarCenterItem>
+      <NavBarRightItem>
+        <div>Right Item</div>
+      </NavBarRightItem>
+    </NavBar>
+    <br />
+    <NavBar>
+      <NavBarLeftItem>
+        <div>Left Contents</div>
+      </NavBarLeftItem>
+      <NavBarCenterItem>
+        <div>Center Item =============================</div>
+      </NavBarCenterItem>
+      <NavBarRightItem>
+        <div>Right Item</div>
+      </NavBarRightItem>
+    </NavBar>
+    <br />
+    <NavBar>
+      <NavBarLeftItem>
+        <div>Left Contents</div>
+      </NavBarLeftItem>
+      <NavBarCenterItem>
+        <div>Center Item</div>
+      </NavBarCenterItem>
+      <NavBarRightItem>
+        <div>Right Item =============================</div>
+      </NavBarRightItem>
+    </NavBar>
+  </div>
+));
